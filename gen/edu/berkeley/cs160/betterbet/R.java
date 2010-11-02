1bad13162762e21dd2c609c7cb2621505b5f5ca5
karthikjagadeesh <karthikjagadeesh@83acf1cd-f339-cf25-4af8-6bb3eab62190>
2010-11-02 01:57:00 +0000

    updates

--- gen/edu/berkeley/cs160/betterbet/R.java
+++ gen/edu/berkeley/cs160/betterbet/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package edu.berkeley.cs160.finalprog;
+package edu.berkeley.cs160.betterbet;
 
 public final class R {
     public static final class attr {
