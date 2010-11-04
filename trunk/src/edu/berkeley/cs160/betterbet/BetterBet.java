a66ef20f74abfebdced8c70f07ff6c4adf93bf14
melissalim89 <melissalim89@83acf1cd-f339-cf25-4af8-6bb3eab62190>
2010-11-04 22:58:25 +0000

    some changes and ignore gen

--- trunk/src/edu/berkeley/cs160/betterbet/BetterBet.java
+++ trunk/src/edu/berkeley/cs160/betterbet/BetterBet.java
@@ -2,12 +2,39 @@ package edu.berkeley.cs160.betterbet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class BetterBet extends Activity {
+	// I'll put this in another class later. -ML
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.play1);
+        
+        Spinner spinner = (Spinner) findViewById(R.id.GroupSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.groups, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
+    
+    public class MyOnItemSelectedListener implements OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent,
+            View view, int pos, long id) {
+          Toast.makeText(parent.getContext(), "The group is " +
+              parent.getItemAtPosition(pos).toString(), Toast.LENGTH_LONG).show();
+        }
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// TODO Auto-generated method stub			
+		}
+    }
+    
+    
 }
