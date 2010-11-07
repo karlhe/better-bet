b2789ef60f040f26235f737e47b13d372f46eb06
melissalim89 <melissalim89@83acf1cd-f339-cf25-4af8-6bb3eab62190>
2010-11-07 01:53:53 +0000

    playactivity

--- trunk/src/edu/berkeley/cs160/betterbet/BetterBet.java
+++ trunk/src/edu/berkeley/cs160/betterbet/BetterBet.java
@@ -4,18 +4,28 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.TabHost;
 
+	
 public class BetterBet extends TabActivity {
+
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.main);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
 	    Resources res = getResources(); // Resource object to get Drawables
 	    TabHost tabHost = getTabHost();  // The activity TabHost
 	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
-	    Intent intent;  // Reusable Intent for each tab
+	    Intent intent; // Reusable Intent for each tab
 
 	    // Create an Intent to launch an Activity for the tab (to be reused)
 	    intent = new Intent().setClass(this, PlayActivity.class);
--- trunk/src/edu/berkeley/cs160/betterbet/PlayActivity.java
+++ trunk/src/edu/berkeley/cs160/betterbet/PlayActivity.java
@@ -1,15 +1,35 @@
 package edu.berkeley.cs160.betterbet;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 
 public class PlayActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        TextView textview = new TextView(this);
-        textview.setText("This is the Play tab");
-        setContentView(textview);
-    }
+		TextView textview = new TextView(this);
+		textview.setText("This is the Play tab");
+		setContentView(R.layout.play1);
+
+		Spinner spinner = (Spinner) findViewById(R.id.GroupSpinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.groups, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+
+		Button instr = (Button)findViewById(R.id.instrButton);
+
+		instr.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				setContentView(R.layout.instructions);
+			}
+		});
+	}
+
+
 }
