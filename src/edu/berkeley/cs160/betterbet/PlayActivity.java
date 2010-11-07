9f1b4c61f81fcd553daedd271b7e6b250a81f02c
karl.he <karl.he@83acf1cd-f339-cf25-4af8-6bb3eab62190>
2010-11-07 05:14:22 +0000

    more cleanup

--- src/edu/berkeley/cs160/betterbet/PlayActivity.java
+++ src/edu/berkeley/cs160/betterbet/PlayActivity.java
@@ -1,6 +1,5 @@
 package edu.berkeley.cs160.betterbet;
 import android.app.Activity;
-import android.app.Application;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
