931d8417ce2d6569d8c4efee55fee7b383aeebd3
melissalim89 <melissalim89@83acf1cd-f339-cf25-4af8-6bb3eab62190>
2010-12-02 04:37:52 +0000

    rules intent

--- src/edu/berkeley/cs160/betterbet/TexasActivity.java
+++ src/edu/berkeley/cs160/betterbet/TexasActivity.java
@@ -1,6 +1,7 @@
 package edu.berkeley.cs160.betterbet;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.os.Bundle;
