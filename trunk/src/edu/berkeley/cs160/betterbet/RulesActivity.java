58d3ef99b0f58ed19fc4abf021919f0ab5a419d0
karthikjagadeesh <karthikjagadeesh@83acf1cd-f339-cf25-4af8-6bb3eab62190>
2010-12-02 04:08:40 +0000

    texas holdem rules

--- trunk/src/edu/berkeley/cs160/betterbet/RulesActivity.java
+++ trunk/src/edu/berkeley/cs160/betterbet/RulesActivity.java
@@ -1,6 +1,10 @@
 package edu.berkeley.cs160.betterbet;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,16 +18,21 @@ public class RulesActivity extends ListActivity {
 
 	  ListView lv = getListView();
 	  lv.setTextFilterEnabled(true);
-	  /*
+	  
 	  lv.setOnItemClickListener(new OnItemClickListener() {
 	    public void onItemClick(AdapterView<?> parent, View view,
 	        int position, long id) {
-	      // When clicked, show a toast with the TextView text
-	      Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+	    	//setRules(position, view);
+	    	Intent i = new Intent().setClass(view.getContext(), TexasActivity.class);
+    		startActivity(i);
+	    	// When clicked, show a toast with the TextView text
+	    	/*
+	    	Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
 	          Toast.LENGTH_SHORT).show();
+	    	*/
 	    }
 	  });
-	  */
+	  
 	}
 	
 	static final String[] COUNTRIES = new String[] {
@@ -35,6 +44,34 @@ public class RulesActivity extends ListActivity {
 	    "Countdown", 
 	    "Follow Queen" 
 	  };
+	
+	public void setRules(int position, View v) {
+    	if (position == 0) {
+    		Intent i = new Intent().setClass(v.getContext(), TexasActivity.class);
+    		startActivity(i);
+    	} 
+    	/*
+    	else if (position == 1) {
+    		Intent i = new Intent().setClass(v.getContext(), OmahaActivity.class);
+    		startActivity(i);
+    	} else if (position == 2) {
+    		Intent i = new Intent().setClass(v.getContext(), SevenCardActivity.class);
+    		startActivity(i);
+    	} else if (position == 3) {
+    		Intent i = new Intent().setClass(v.getContext(), FiveCardActivity.class);
+    		startActivity(i);
+    	} else if (position == 4) {
+    		Intent i = new Intent().setClass(v.getContext(), CaribbeanActivity.class);
+    		startActivity(i);
+    	} else if (position == 5) {
+    		Intent i = new Intent().setClass(v.getContext(), CountdownActivity.class);
+    		startActivity(i);
+    	} else if (position == 6) {
+    		Intent i = new Intent().setClass(v.getContext(), FollowQueenActivity.class);
+    		startActivity(i);
+    	} 
+    	*/
+    }
     /*
     private OnClickListener fiveCardButtonListener = new OnClickListener () {
     	public void onClick(View v) {
