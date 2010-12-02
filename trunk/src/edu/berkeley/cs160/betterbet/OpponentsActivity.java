f3f623644708e35fcaeb3098d0c81118aad4f465
melissalim89 <melissalim89@83acf1cd-f339-cf25-4af8-6bb3eab62190>
2010-12-02 00:56:57 +0000

    opponents work in progress

--- trunk/src/edu/berkeley/cs160/betterbet/OpponentsActivity.java
+++ trunk/src/edu/berkeley/cs160/betterbet/OpponentsActivity.java
@@ -1,16 +1,35 @@
 package edu.berkeley.cs160.betterbet;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class OpponentsActivity extends Activity {
+public class OpponentsActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView textview = new TextView(this);
-        textview.setText("This is the Opponents tab");
-        setContentView(textview);
-    }
-
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+         
+        String[] countries = getResources().getStringArray(R.array.opponentsArray);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.opponents, countries));
+        
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                int position, long id) {
+              // When clicked, show a toast with the TextView text
+              Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                  Toast.LENGTH_SHORT).show();
+            }
+          });
+        }
 }
+
+ 
